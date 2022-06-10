d2f1fc8af7deb1d07da2ba4ab2fe8fc2330ec75a
dev-dwr <dawid.rymarczyk02@gmail.com>
2022-06-10 10:40:55 +0200

    added calculator for tests, changed readme.md file

--- README.md
+++ README.md
@@ -16,6 +16,9 @@ Live template ulatwia nam pewne powtarzalne rzeczy w IntelliJ
 3. wybieramy Configure Live Templates
 4. naciskamy plusika i dodajemy nowy Live Template
 
+### Uruchomianie testów przy pomocy IntelliJ, a nie narzędzia automatyzującego
+1. Ustawienia -> Build,Execution,Deployment -> Build Tools -> Gradle/Maven
+2. Ustaw pole "Run tests using" na IntelliJ IDEA
 
 ### Przydatne linki:
 * [Dokumentacja JUnit 5](https://junit.org/junit5/docs/current/user-guide/)
---
+++ src/main/java/ex2/Calculator.java
@@ -0,0 +1,22 @@
+package ex2;
+
+public class Calculator {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static float divide(float a, float b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by 0");
+        }
+        return a / b;
+    }
+}
